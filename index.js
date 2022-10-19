297193c9f7aed728cf2e3d8f42064f932e1c1fa6
Tiffany Cheng <tiffanycheng.001@gmail.com>
2022-10-18 18:21:42 -0600

    commit tree 0bd5c80d2a28a8628680ff8447b67291bcc6cd41

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(pickupLocation - 42)
+}
+
+function distanceFromHqInFeet(blocksToFeet){
+    distanceFromHqInBlocks(blocksToFeet)
+    return distanceFromHqInBlocks(blocksToFeet) * 264
+}
+
+function distanceTravelledInFeet(num1,num2){
+    return Math.abs(num1-num2) * 264
+}
+
+function calculatesFarePrice(num1,num2){
+    if (distanceTravelledInFeet(num1,num2) < 400){
+        return 0
+    }
+    if (distanceTravelledInFeet(num1,num2) > 400 && distanceTravelledInFeet(num1,num2) < 2000){
+        return (distanceTravelledInFeet(num1,num2) - 400) * .02
+    }
+    if (distanceTravelledInFeet(num1,num2) > 2000 && distanceTravelledInFeet(num1,num2) < 2500){
+        return 25
+    }
+    if (distanceTravelledInFeet(num1,num2) > 2500){
+        return 'cannot travel that far'
+    }
+}
+
